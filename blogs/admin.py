4fa1a40fd5dc2cdf93d256b887a413f20be34679
AlexisDongMariano <65661143+AlexisDongMariano@users.noreply.github.com>
2020-08-22 22:50:35 +0800

    Delete admin.py

--- blogs/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from blogs.models import BlogPost2, BlogComment2, Like
-
-admin.site.register(BlogPost2)
-admin.site.register(BlogComment2)
-admin.site.register(Like)
